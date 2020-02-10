60bcd84b82bb6af995b09228fc14b97cc1114735
John Valai <git@jvk.to>
2020-02-10 11:19:58 +0100

    💩 Temporary clean-up of home view – needs to be re-factored and split into separate components

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -27,28 +27,43 @@ export function home() {
     // cleanup:
     return () => {
       typed.destroy();
-      // GameSFX.destroy();
     }
 
   });
 
   return (
-    <>
-      <div>home</div>
-      <Button 
-        route="/game"
-        label="Play" 
-      />
-    </>
+    <div className="h-100 d-flex align-items-center">
+      <div className="container-sm">
+
+        <Logo />
+
+        <section className="message">
+          <div 
+            className="nes-balloon from-left d-block"
+            style={{ minHeight: "4.3em" }}
+          >
             <div className="typed-src d-none">
               <p>This game has been modeled on Google's <a href="https://quickdraw.withgoogle.com/data">Quick, Draw!</a>^2000</p>
               <p>^1000Brought to you by <span className="text-primary">@jvkdev</span> and <span className="text-danger">Super Mari</span>^500</p>
               <p>Brought to you by <span className="text-primary">@jvkdev</span> and <span className="text-danger">EPFL Extension School</span> <i className="nes-icon heart align-middle"></i></p>
             </div>
             <div><span className="typed-msg"></span></div>
+          </div>
+        </section>
+
+        <i className="nes-octocat animate float-left"></i>
+        <Button 
+          route="/game" 
           click={() => {
             GameSFX.play("click");
             GameSFX.play("theme");
           }}
+          label="Play Game >" 
+          className="float-right mt-3"
+          buttonClassName="is-success"
+        />
+
+      </div>
+    </div>
   );
 }
