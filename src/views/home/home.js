2b5d06e726fa9d966f9315d65aabf5ac3388aac0
John Valai <git@jvk.to>
2020-02-10 11:06:46 +0100

    ✨ add typewrite effect for home view text

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -3,8 +3,16 @@ import { Button } from "components/SketchGame/components/Button";
 import { Logo } from "components/SketchGame/components/Logo";
 import { GameSFX } from "components/SketchGame/utils/sounds.js";
 
-export const home = () => {
-import { GameSFX } from "../../utils/sounds.js";
+import Typed from 'typed.js';
+const typedConfig = {
+  typeSpeed: 20,
+  backSpeed: 10,
+  backDelay: 50,
+  startDelay: 500
+};
+
+export function home() {
+
   // using useEffect from the router, seems it's the reason the linter doesn't 
   // think this fn isn't React – hacky fix for now:
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -23,6 +31,7 @@ import { GameSFX } from "../../utils/sounds.js";
     }
 
   });
+
   return (
     <>
       <div>home</div>
@@ -31,6 +40,12 @@ import { GameSFX } from "../../utils/sounds.js";
         label="Play" 
       />
     </>
+            <div className="typed-src d-none">
+              <p>This game has been modeled on Google's <a href="https://quickdraw.withgoogle.com/data">Quick, Draw!</a>^2000</p>
+              <p>^1000Brought to you by <span className="text-primary">@jvkdev</span> and <span className="text-danger">Super Mari</span>^500</p>
+              <p>Brought to you by <span className="text-primary">@jvkdev</span> and <span className="text-danger">EPFL Extension School</span> <i className="nes-icon heart align-middle"></i></p>
+            </div>
+            <div><span className="typed-msg"></span></div>
           click={() => {
             GameSFX.play("click");
             GameSFX.play("theme");
