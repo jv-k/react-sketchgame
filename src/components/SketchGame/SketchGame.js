1b6815daa3a610e7a2470992d9d9cd672aef1a97
John Valai <git@jvk.to>
2020-02-10 11:29:07 +0100

    🚧 Reworked Logo and game cards to be separate before big architectural change

--- src/components/SketchGame/SketchGame.js
+++ src/components/SketchGame/SketchGame.js
@@ -5,28 +5,65 @@ import { Controls } from "./components/Controls";
 import { useSketchGame } from "./hooks";
 
 import "./SketchGame.css";
-import "./hooks";
-
-import { loadModel, loadLabels } from  "../../utils";
-
-// load data
-const model = loadModel("./model/model.json");
-const labels = require("./labels.json");
+import "nes.css/css/nes.min.css";
 
 // let ref = React.createRef();
 
-// config options
+const ScoreCard = (props) => {
+  return(
+    <div className="nes-container is-centered is-rounded mb-3">
+      <p className="">Round 1 of 10</p>
+      <progress className="m-0 nes-progress is-success" value="20" max="100" style={{height: "1em"}}></progress>
+    </div>
+  );
+}
+
+const AnswerCard = (props) => {
+  return(
+    <div className="nes-container is-rounded mb-3" style={{ width: "300px", height: "300px"}}></div>  
+  );
+}
+
+const QuestionCard = (props) => {
+  return(
+    <>
+      <div className="nes-balloon from-left text-left mt-1">
+        <p className="msgTxt">Game text,ß play it Game text, play it Game text, play it !</p>
+      </div>
+      <i className="nes-mario"></i>
+    </>
+  );
+}
 
 class SketchGame extends React.Component {
 
   render() {  
     return(  
       <>
+        <Logo />      
+        <div className="nes-container is-rounded" style={{ backgroundColor: "rgba(0,0,0,0)"}} >
+          <div className="row">
+            <div className="col">
+              <ScoreCard />
+            </div>
+          </div>
 
+          <div className="row">
+            <div className="col">
+              <AnswerCard />
+            </div>
+            <div className="col">
+              <QuestionCard />
+            </div>
+          </div>
+          
+          <div className="row">
+            <Controls />
+          </div>
+        </div>
       </>
     );
   }
-
 }
 
 export { SketchGame };
