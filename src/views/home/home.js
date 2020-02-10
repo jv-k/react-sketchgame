b65d39e08692a2fbec031923c72af4cab4f03dd6
John Valai <git@jvk.to>
2020-02-10 05:31:46 +0100

    🚨 remove linter warning in home.js view, was getting flagged for not being a React fn comp as was called by router (?)

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -3,6 +3,9 @@ import { Button } from "../../components/shared/Button.js";
 
 export const home = () => {
 import { GameSFX } from "../../utils/sounds.js";
+  // using useEffect from the router, seems it's the reason the linter doesn't 
+  // think this fn isn't React – hacky fix for now:
+    // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
 
     // mount:
