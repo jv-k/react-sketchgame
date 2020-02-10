33e0bbdaffe1302860248f571db9f18979a503f7
John Valai <git@jvk.to>
2020-02-10 11:30:33 +0100

    ♻️ Refactored game controls component for clarity before big architectural change

--- src/components/SketchGame/components/Controls/Controls.js
+++ src/components/SketchGame/components/Controls/Controls.js
@@ -1,35 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { getPrediction } from "../Utils";
+import React from "react";
 import { Button } from "../Button";
+import { GameSFX } from "../../utils/sounds.js";
 
-const Controls = ({ theCanvas, model, labels }) => {
-  let [prediction, setPrediction] = useState(""); // Sets default label to empty string.
-
-  useEffect(() => {
-    console.log(prediction);
-  });
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          const canvas = theCanvas.current;
-          const ctx = canvas.getContext("2d");
-          ctx.fillRect(0, 0, canvas.height, canvas.width);
-        }}
-      >
-        Clear the canvas.
-      </button>
-      <button
-        onClick={() =>
-          getPrediction(theCanvas, model).then(prediction =>
-            setPrediction(labels[prediction[0]])
-          )
-        }
-      >
-        Predict the drawing.
-      </button>
-    </div>
+export const Controls = () => {
+  return(
+    <>
+      <div className="col">
+        <Button 
+          label="Clear" 
+          click={() => { 
+            GameSFX.play("clear");
+          }} 
+          className="is-warning"
+        /> 
+      </div>
+      <div className="col">
+        <Button 
+          label="Quit!" 
+          route="/home" 
+          click={() => { 
+            GameSFX.silence();
+            GameSFX.play("quit");
+          }}
+          className="float-right"
+        />  
+      </div>
+    </>
   );
 }
-export { Controls };
