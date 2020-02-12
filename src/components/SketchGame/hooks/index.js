b583b2cdd6d225b37f5f8587cf2af2549e844dd2
John Valai <git@jvk.to>
2020-02-12 02:09:24 +0100

    ✨ add userTimer hook

--- src/components/SketchGame/hooks/index.js
+++ src/components/SketchGame/hooks/index.js
@@ -1,4 +1,4 @@
 // An entry file that merely re-exports its siblings to keep things neat.
 export * from './useGameContext';
 export * from './useRoundContext';
-export * from './useRounds';
+export * from './useRounds';export * from './useTimer';
---
+++ src/components/SketchGame/hooks/useTimer.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+export const useTimer = (timeToCountDown, callBack) => {
+  const [timeLeft, setTimeLeft] = useState(timeToCountDown);
+  const [isActive, setIsActive] = useState(false);
+
+  function start() {
+    setIsActive(true);
+  }
+
+  function stop() {
+    setIsActive(false);
+  }
+
+  function reset() {
+    stop();
+    setTimeLeft(timeToCountDown);
+  }
+
+  useEffect(() => {
+    if (isActive) {
+      const interval = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+
+      if (timeLeft === 0) {
+        stop();
+        callBack.call();
+        return;
+      }
+  
+      // clear interval on re-render to avoid memory leaks
+      return () => clearInterval(interval);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isActive, timeLeft]);
+
+  return { 
+    timeLeft: timeLeft, 
+    startTimer: () => start(),
+    stopTimer: () => stop(),
+    resetTimer: ()=> reset()
+  }
+};
