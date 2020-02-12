a8afe3b7fc39cc419419eded56bae480025a5f46
John Valai <git@jvk.to>
2020-02-12 02:13:58 +0100

    🚧 Separate GameRound component + 🧹

--- src/components/SketchGame/SketchGame.js
+++ src/components/SketchGame/SketchGame.js
@@ -1,15 +1,42 @@
-import React from "react";
-import { Logo } from "./components/Logo";
-import { Controls } from "./components/Controls";
+import React, { useReducer, useState } from "react";
+
+import { Logo }      from "./components/Logo";
+import { Controls }  from "./components/Controls";
 
 import { GameProvider } from "./providers";
 import { gameReducer }  from "./reducers";
+import { useRounds } from "./hooks";
 
+// styles
 import "./SketchGame.css";
 import "nes.css/css/nes.min.css";
 
-// let ref = React.createRef();
+export const SketchGame = ({ config, model }) => {
 
+  const initialConfig = {
+    score: 0,
+    labels: config.labels, // we'll randomize this later, so it should be in state
+    currentRound: 1
+  }
+  const [ state, dispatch ] = useReducer(gameReducer, { ...initialConfig });
+  
+  // In my implementation there's only one clear canvas btn as opposed to one
+  // for each round container, which makes life harder when it comes to clearing the canvas,
+  // (for practice!). So the following is the way I used to tell the GameContext
+  // what the current visible canvas ref is for this purpose:
+  const [ curCanvasRef, setCurCanvasRef ] = useState({});
+
+  console.log("ROOT");
+ 
+  // TODO: 
+  // – start game method
+  // – reset game method
+  const { rounds } = useRounds({ ...config });
+
+  return(  
+    <>
+      <Logo />      
+      <div className="nes-container is-rounded p-2" style={{ backgroundColor: "rgba(0,0,0,0)"}} >
 
         <GameProvider value={{ ...config, state, dispatch, model, curCanvasRef, setCurCanvasRef }}>
           { rounds }
@@ -20,37 +47,3 @@ import "nes.css/css/nes.min.css";
     </>
   );
 }
-
-class SketchGame extends React.Component {
-
-  render() {  
-    return(  
-      <>
-        <Logo />      
-        <div className="nes-container is-rounded" style={{ backgroundColor: "rgba(0,0,0,0)"}} >
-          <div className="row">
-            <div className="col">
-              <ScoreCard />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col">
-              <AnswerCard />
-            </div>
-            <div className="col">
-              <QuestionCard />
-            </div>
-          </div>
-          
-          <div className="row">
-            <ScoreCard />
-            <Controls />
-          </div>
-        </div>
-      </>
-    );
-  }
-}
-
-export { SketchGame };
---
+++ src/components/SketchGame/components/GameRound/GameRound.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+
+import { RoundProvider } from "../../providers";
+import { useGameContext } from  "../../hooks";
+import { useTimer } from  "../../hooks";
+
+import { ScoreCard } from  "../ScoreCard";
+import { AnswerCard } from "../AnswerCard";
+import { QuestionCard } from "../QuestionCard";
+
+let canvasRef = React.createRef();
+
+export const GameRound = ({ index, label, hideRound, timeLimit }) => {
+
+  // set-up timer
+  const { timeLeft, startTimer, stopTimer } = useTimer(
+    timeLimit, 
+    ()=>{  // callback for when timer is done
+      console.log("TIMER DONE!");
+    }
+  );
+
+  // this is for the control button in the GameContext
+  const { setCurCanvasRef } = useGameContext();
+
+  useEffect(() => {
+    setCurCanvasRef(canvasRef);
+    startTimer();        
+    return () => stopTimer(); // cleanup:
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <RoundProvider value={{ index, label, hideRound, timeLeft, canvasRef }}>
+      
+      <div className="row">
+        <div className="col">
+          <ScoreCard />
+        </div>
+      </div>
+
+      <div className="row align-items-end">  
+        <div className="col">          
+          <AnswerCard ref={ canvasRef } />
+        </div>
+        <div className="col mr-3 mb-2">
+          <QuestionCard 
+            label={ label }
+            timeLeft={ timeLeft }
+          />
+        </div>
+      </div>       
+
+    </RoundProvider>      
+  );
+};
