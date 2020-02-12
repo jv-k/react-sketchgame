956908a2ae59989b4b0e6b1a880b3155b352d622
John Valai <git@jvk.to>
2020-02-12 02:01:11 +0100

    🎨 Add ability to clear current canvas from shared controls

--- src/components/SketchGame/components/Controls/Controls.js
+++ src/components/SketchGame/components/Controls/Controls.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Button } from "../Button";
+import { clearCanvas } from "../Canvas";
 import { GameSFX } from "../../utils/sounds.js";
+import { useGameContext } from  "../../hooks";
 
 export const Controls = () => {
+
+  const { curCanvasRef } = useGameContext();
+  
   return(
     <div className="row">
       <div className="col">
-        <Button 
+      <Button 
           label="Clear" 
           click={() => { 
+            clearCanvas(curCanvasRef);
             GameSFX.play("clear");
           }} 
           className="is-warning"
         /> 
       </div>
-      <div className="col">
+      <div className="col"> 
         <Button 
           label="Quit!" 
           route="/home" 
