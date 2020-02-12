1e084a51ef5fb1e15ca7f3609a654a8f20183064
John Valai <git@jvk.to>
2020-02-12 01:12:10 +0100

    ✨ Separate QuestionCard component

--- src/components/SketchGame/SketchGame.js
+++ src/components/SketchGame/SketchGame.js
@@ -23,13 +23,7 @@ const ScoreCard = (props) => {
           <Controls />
         </GameProvider>
 
-const QuestionCard = (props) => {
-  return(
-    <>
-      <div className="nes-balloon from-left text-left mt-1">
-        <p className="msgTxt">Game text,ß play it Game text, play it Game text, play it !</p>
       </div>
-      <i className="nes-mario"></i>
     </>
   );
 }
--- src/components/SketchGame/components/QuestionCard/QuestionCard.js
+++ src/components/SketchGame/components/QuestionCard/QuestionCard.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-export const QuestionCard = (props) => {
+import Typed from 'typed.js';
+const typedConfig = {
+  typeSpeed: 20,
+  backSpeed: 10,
+  backDelay: 50,
+  startDelay: 500
+};
+
+export const QuestionCard = ({ label, timeLeft }) => {
+
+  const ref = React.createRef();
+
+  useEffect(() => {
+    let typed = new Typed(  
+      '.msgTxt',
+      { 
+        strings: [ getQuestion(label, timeLeft, ref) ], 
+        ...typedConfig,
+        onComplete: (self) => {
+        }
+      }
+    );
+
+    // cleanup:
+    return () => {
+      typed.destroy();
+    }
+  }, []);
+ 
   return(
     <>
-      <div className="nes-balloon from-left text-left mt-1">
-        <p className="msgTxt">Game text,ß play it Game text, play it Game text, play it !</p>
+      <div className="nes-balloon from-right w-100 mt-1">
+          <span className="msgTxt"></span>
       </div>
-      <i className="nes-mario"></i>
+      <i className="nes-mario reflect-h float-right"></i>
     </>
   );
+}
+
+function getQuestion(label, timeLeft, ref) {
+  return "Can you draw a <span class=\"text-danger\">"+label+"</span>..? " 
+  + "^1000<br/>You have <span class=\"text-danger\" ref=\"" + ref + "\">" + timeLeft +"</span> seconds to draw it in the box. ^1000GO!";  
 }
