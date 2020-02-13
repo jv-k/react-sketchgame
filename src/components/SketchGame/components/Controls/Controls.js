16582ca2356984bab54edd5d5d7ffc4fd59c5dac
John Valai <git@jvk.to>
2020-02-14 00:07:02 +0100

    ✨ Add dialog for quit feature!

--- src/components/SketchGame/components/Controls/Controls.js
+++ src/components/SketchGame/components/Controls/Controls.js
@@ -24,14 +24,30 @@ export const Controls = ({ canvasRef, NextBtnCallBack }) => {
       </div>
       <div className="col"> 
         <Button 
-          label="Quit!" 
-          route="/home" 
-          click={() => { 
-            GameSFX.silence();
-            GameSFX.play("quit");
+          label="Quit" 
+          click={() => {
+            GameSFX.play("click");
+            document.getElementById('dialog-rounded').showModal();
           }}
           className="float-right"
         />  
+        <dialog className="nes-dialog is-rounded" id="dialog-rounded">
+          <form method="dialog">
+            <p>Are you sure?!</p>
+            <menu className="dialog-menu">
+              <Button 
+                label="Quit!"
+                route="/home"
+                className="nes-btn is-primary"
+                click={() => { 
+                  GameSFX.silence();
+                  GameSFX.play("quit");
+                }}
+              />
+              <button className="nes-btn">Cancel</button>
+            </menu>
+          </form>
+        </dialog>        
       </div>
     </div>
   );
