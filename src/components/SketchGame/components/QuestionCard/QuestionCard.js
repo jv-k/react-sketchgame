578851565d05ac82d9657740687a4dbdc11fe072
John Valai <git@jvk.to>
2020-02-14 08:57:17 +0100

    🎨 Questions are auto-generated based on previous win/loss

--- src/components/SketchGame/components/QuestionCard/QuestionCard.js
+++ src/components/SketchGame/components/QuestionCard/QuestionCard.js
@@ -1,49 +1,84 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { GameContext } from "../../providers";
+import { getRandomNumber } from "../../utils";
 
 import Typed from 'typed.js';
-const typedConfig = {
-  typeSpeed: 20,
-  backSpeed: 10,
-  backDelay: 50,
-  startDelay: 500
-};
 
-export const QuestionCard = ({ label, timeLeft }) => {
+export class QuestionCard extends React.Component {
 
-  const ref = React.createRef();
+  static contextType = GameContext;
 
-  useEffect(() => {
-    let typed = new Typed(  
-      '.msgTxt',
-      { 
-        strings: [ getQuestion(label, timeLeft, ref) ], 
-        ...typedConfig,
-        onComplete: (self) => {
-        }
+  constructor(props) {
+    super();
+    this.state = {
+      timeRef: React.createRef(),
+      isTyped: false,
+      typed: null
+    }
+    this.typed = {};
+  }
+
+  getQuestion = () => {
+    const { wonLastRound } = this.context;
+    const msgs = this.context.msgs;
+    const isFirstRound = (this.context.currentRound === 0);
+
+    const intro = (() => {
+      if (isFirstRound) 
+        return msgs.intro
+      else if (wonLastRound) 
+        return msgs.pointScore[getRandomNumber(msgs.pointScore.length)]
+      else
+        return msgs.pointLose[getRandomNumber(msgs.pointLose.length)]
+    })();
+
+    return  intro 
+          + msgs.pause 
+          + msgs.question
+            .replace("{{label}}", this.props.label)
+            .replace("{{timeLeft}}", this.props.timeLeft)
+            .replace("{{timeLeftRef}}", this.timeLeft)
+          + (isFirstRound ? msgs.questionSuffix : "!");
+  }
+
+  componentDidUpdate() {
+    // since the element is inside the typed element, we can only perfectly
+    // reference it directly via DOM (easiest method I found):
+    const el = document.getElementById("timeLeft");
+    if (el !== null) el.innerText = this.props.timeLeft;
+  }
+  
+  componentDidMount() {
+    const typedConfig = { typeSpeed: 20 };
+    
+    // start text animation
+    this.typed = new Typed(  
+      '.msgTxt', { 
+        strings: [
+          this.getQuestion()
+        ], 
+        ...typedConfig, 
         onComplete: (self) => { 
           // tell GameRound provider to start the timer when text typing is done:
           this.props.callBack();
         } 
       }
     );
+  }
 
-    // cleanup:
-    return () => {
-      typed.destroy();
-    }
-  }, []);
- 
-  return(
-    <>
-      <div className="nes-balloon from-right w-100 mt-1">
-          <span className="msgTxt"></span>
-      </div>
-      <i className="nes-mario reflect-h float-right"></i>
-    </>
-  );
+  componentWillUnmount() {
+    this.typed.destroy();
+  }
+      
+  render() {
+    return(
+      <>
+        <div className="nes-balloon from-right w-100 mt-1">
+            <span className="msgTxt" ref={this.timeLeft}></span>
+        </div>
+        <i className="nes-mario reflect-h float-right"></i>
+      </>
+    )
+  }
 }
 
-function getQuestion(label, timeLeft, ref) {
-  return "Can you draw a <span class=\"text-danger\">"+label+"</span>..? " 
-  + "^1000<br/>You have <span class=\"text-danger\" ref=\"" + ref + "\">" + timeLeft +"</span> seconds to draw it in the box. ^1000GO!";  
-}
