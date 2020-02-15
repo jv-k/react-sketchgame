14c2bf233ddbe26903b7e709bc10929335e69169
John Valai <git@jvk.to>
2020-02-15 18:14:03 +0100

    🥅 Add error handling capability to show error screen for results view – When navigating to the results view manually, the router's state isn't passed as it usually is by the game component, this produces errors – By wrapping the component in an error handler (thanks to Dan Abramov for the sketch) we now show an error message.

--- src/routes.js
+++ src/routes.js
@@ -15,8 +15,46 @@ export const Routes = () => {
         </Route>
         <Route path="/home" component={ Home } />
         <Route path="/game" component={ Game } />
-        <Route path="/result" component={ Result } />
+        <ErrorBoundary> 
+          {/* Handle error: navigating to /result will result in an error,
+              as props.location isn't passed, normally it's passed by the router from /game */}
+          <Route path="/result" component={ Result } />
+        </ErrorBoundary>          
       </Switch>
     </>
   )
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+  
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+  }
+  
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <>
+          <div className="h-100 d-flex align-items-center">
+            <div className="container-sm text-center">
+              <h2>Oops.... </h2>
+              <h2>Something went wrong!</h2>
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      // No error, just render children
+      return this.props.children;
+    }
+  }  
 }
