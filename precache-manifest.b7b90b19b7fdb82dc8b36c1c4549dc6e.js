e12ed9d9b73ca69d67660ebf4f21f4ea098d2092
John Valai <git@jvk.to>
2021-09-10 16:15:29 +0200

    Updates

---
+++ precache-manifest.b7b90b19b7fdb82dc8b36c1c4549dc6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c291a80f4f8ca6a23dd0b6d88bb80c0d",
+    "url": "/react-sketchgame/index.html"
+  },
+  {
+    "revision": "2f559cd95e791d10b649",
+    "url": "/react-sketchgame/static/css/2.e3843c74.chunk.css"
+  },
+  {
+    "revision": "e6bc8d6ecccbf878e836",
+    "url": "/react-sketchgame/static/css/main.450ef2d6.chunk.css"
+  },
+  {
+    "revision": "2f559cd95e791d10b649",
+    "url": "/react-sketchgame/static/js/2.79e845b6.chunk.js"
+  },
+  {
+    "revision": "8e566d777d5eea46083295a1450cce7d",
+    "url": "/react-sketchgame/static/js/2.79e845b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bc8d6ecccbf878e836",
+    "url": "/react-sketchgame/static/js/main.9e182405.chunk.js"
+  },
+  {
+    "revision": "7b769ecb34457b2520f8",
+    "url": "/react-sketchgame/static/js/runtime-main.6744019d.js"
+  },
+  {
+    "revision": "9e4223b29d28adc75b616354ff840800",
+    "url": "/react-sketchgame/static/media/8bit-bg-1.9e4223b2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45d569f4f95d3fa555526988d644e4b1.js"
+  "/react-sketchgame/precache-manifest.b7b90b19b7fdb82dc8b36c1c4549dc6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sketchgame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
