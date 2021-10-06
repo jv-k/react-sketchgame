6f622a600a6bfe85406b57ca9b76320bd4665353
John V <git@jvk.to>
2021-10-06 15:08:14 +0200

    Updated project description, for clarification

--- src/index.js
+++ src/index.js
@@ -9,29 +9,28 @@ Homepage:
   https://github.com/jv-k/react-sketchgame
 
 Description:
-  Here are some of the rules which I followed, in order to create a discoverable, neat, easy to maintain+test+debug,
-  and to follow the paradigm of separation of concerns:
+  Here are some of the rules which I followed, in order to create discoverable, neat, easy to maintain+test+debug code,
+  and to follow the paradigm of separation of concerns
 
-VIEWS + ROUTES
-  – Routing logic is placed insde router.js
-  – Each page is a react component, in the views folder
-  – Each view has a route that triggers it
-  – A page component uses other components to assemble the page like lego blocks
-  – Route logic is mostly non-reusable, means view components are not reusable,
-    but we want the components inside the components directory to be reusable
+  VIEWS + ROUTES
+    – Routing logic is placed insde router.js
+    – Each page is a react component, in the views folder
+    – Each view has a route that triggers it
+    – A page component uses other components to assemble the page like lego blocks
+    – Route logic is mostly non-reusable, means view components are not reusable,
+      but we want the components inside the components directory to be reusable
 
-COMPONENTS
-  – All components are inside components directory
-  - All components are inside a dedicated components directory, in a separate file. 
-  – Components are highly discoverable and neat and easily re-usable
-  – Each components has it's main file with the same Pascal-case name
-  – Each component doesn't use default export, it avoid confusion when importing
-    – Rather, it's exported in it's own index.js and neatly imported without using the .js extension
-  – Highly re-usable components kept in shared folder
-  - Components are functional, unless they really need to be class-based
-  – Presentational components are provided data via props/context
-  – Container component w/game logic control the state, and contain presentation components
-  – 
+  COMPONENTS
+    – All components are inside the components directory.
+    - Each components are inside a dedicated fold in src/components, in separate files. 
+    – Components are highly discoverable and neat and easily re-usable.
+    – Each components has it's main file with the same Pascal-case name.
+    – Each component doesn't use default export, to avoid any confusion when importing.
+      – Rather, they're exported in their own index.js and then neatly imported without using the .js extension
+    – Highly re-usable components are kept in a shared folder
+    - Components are functional, unless they really need to be class-based
+    – Presentational components are provided data via props/context
+    – Container components w/game logic control the state, and contain presentation components
 */
 
 import React from "react";
